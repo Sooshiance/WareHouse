9ef0f7e483339204cf7563cf85e26157f7841295
Sooshiance <s.katafract@gmail.com>
2023-12-16 00:13:58 +0400

    Update urls.py

--- user/urls.py
+++ user/urls.py
@@ -1,16 +1,21 @@
 from django.urls import path
+from django.contrib.auth.views import PasswordResetView, PasswordResetConfirmView, PasswordResetCompleteView
 
-from .views import (
-    LoginPageView, LogoutPageView, SignupPageView, PasswordChangePageView, PasswordResetConfirmViewPage,
-    PasswordResetCompleteViewPage
-)
+from .views import (LoginPageView, LogoutPageView, SignupPageView)
 
 
 urlpatterns = [
     path('', LoginPageView.as_view(), name='LOGIN'),
     path('logout/', LogoutPageView.as_view(), name='LOGOUT'),
     path('register/', SignupPageView.as_view(), name='REGISTER'),
-    path('change/', PasswordChangePageView.as_view(), name='CHANGE'),
-    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmViewPage.as_view(), name='CONFIRM'),
-    path('password-reset-complete/', PasswordResetCompleteViewPage.as_view(), name='password_reset_complete'),
+    path('change/', PasswordResetView.as_view(template_name ="pass_change.html",
+                                                   email_template_name = "pass_change_email.html",
+                                                   subject_template_name = "password_reset_subject.txt",
+                                                  success_message ="""  We've emailed you instructions for setting your password,
+                      if an account exists with the email you entered. You should receive them shortly.
+                      If you don't receive an email,
+                      please make sure you've entered the address you registered with, and check your spam folder. """,
+                                                  success_url = reverse_lazy("/")), name='CHANGE'),
+    path('password-reset-confirm/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name = "password_reset_confirm.html"), name='CONFIRM'),
+    path('password-reset-complete/', PasswordResetCompleteView.as_view(template_name = "password_reset_complete.html"), name='password_reset_complete'),
 ]
